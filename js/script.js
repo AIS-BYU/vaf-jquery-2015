25226b584024df2fe3e0a3bbe122e53edcae35fb
Carlos Filoteo <c.filoteo@byu.edu>
2015-02-15 19:57:16 -0700

    Added files

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/**
+ * PROTIP: the below DOM .ready() can be written even shorter.
+ * The $ namespace will call $.ready() by default. .
+ *
+ * $(function(){
+ *    //Your code here
+ * })
+ *
+ */
+
+$(document).ready(function(){
+  /** DOM ready
+   * All code that accesses the DOM should be in ready handler.
+   **/
+
+
+  /** EXERCISE 1
+
+   Select notification
+   Fade out on one of these events
+   - click
+   - dblclick
+   - mouseenter
+
+
+   Select dropdown button
+   Toggle dropdown menu on click event
+
+   **/
+
+   /** EXERCISE 2
+
+   Pull data from /data/customer.json
+   Loop through each record to create a row
+   Select the table body element
+   Append that row
+
+    */
+});
